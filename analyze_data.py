d6257f82c796a2990197c789fef2426c3fdaa47e
Yang Liu <foamliu@yeah.net>
2018-09-06 17:29:24 +0800

    update

--- analyze_data.py
+++ analyze_data.py
@@ -1,11 +1,10 @@
-import os
+import random
 import xml
-from collections import Counter
 
 import nltk
-from gensim.models import KeyedVectors
+import numpy as np
 from tqdm import tqdm
-
+import jieba
 from config import train_translation_path, valid_translation_en_filename, valid_translation_zh_filename
 
 
@@ -25,13 +24,17 @@ def count_train_samples():
         eng_sen_list.append(eng_sen)
         chn_sen_list.append(chn_sen)
     print('len(eng_sen_list): ' + str(len(eng_sen_list)))
-    print(eng_sen_list[0])
-    print(chn_sen_list[0])
+
+    length = len(eng_sen_list)
+    sample_id = random.randint(0, length - 1)
+    print(eng_sen_list[sample_id])
+    print(chn_sen_list[sample_id])
 
 
 def count_valid_samples():
     print('counting valid samples')
 
+    ## fix parsing issues
     with open(valid_translation_en_filename, 'r') as f:
         data_en = f.readlines()
     data_en = [line.replace('&', '&amp;') for line in data_en]
@@ -50,75 +53,49 @@ def count_valid_samples():
     data_zh = [elem.text.strip() for elem in root.iter() if elem.tag == 'seg']
     print('len(data_en): ' + str(len(data_en)))
     print('len(data_zh): ' + str(len(data_zh)))
-    print(data_en[0])
-    print(data_zh[0])
+
+    length = len(data_en)
+    sample_id = random.randint(0, length - 1)
+    print(data_en[sample_id])
+    print(data_zh[sample_id])
 
 
-def train_length_en():
-    print('train_length_en')
-    translation_path = os.path.join(train_translation_folder, train_translation_en_filename)
-    with open(translation_path, 'r') as f:
-        data = f.readlines()
+def count_train_length_en():
+    print('counting train length en')
+    with open(train_translation_path, 'r') as f:
+        lines = f.readlines()
 
-    max_len = 0
     lengthes = []
     print('scanning train data (en)')
-    for sentence in tqdm(data):
-        tokens = nltk.word_tokenize(sentence.strip().lower())
+    for line in tqdm(lines):
+        tokens = line.split('\t')
+        eng_sen = tokens[2].strip().lower()
+        tokens = nltk.word_tokenize(eng_sen)
         length = len(tokens)
         lengthes.append(length)
-        if length > max_len:
-            max_len = length
 
-    print('max_len: ' + str(max_len))
-
-    counter_length = Counter(lengthes)
-    total_count = 0
-    common = counter_length.most_common()
-    covered_count = 0
-    for i in range(1, max_len + 1):
-        count = [item[1] for item in common if item[0] == i]
-        if count:
-            covered_count += count[0]
-        print('{} -> {}'.format(i, covered_count / total_count))
+    print('max_len: ' + str(np.max(lengthes)))
 
 
-def train_vocab_en():
-    print('train_vocab_en')
-    print('loading fasttext en word embedding')
-    word_vectors = KeyedVectors.load_word2vec_format('data/wiki.en.vec')
-    translation_path = os.path.join(train_translation_folder, train_translation_en_filename)
+def count_train_length_zh():
+    print('counting train length en')
+    with open(train_translation_path, 'r') as f:
+        lines = f.readlines()
 
-    with open(translation_path, 'r') as f:
-        data = f.readlines()
+    lengthes = []
+    print('scanning train data (en)')
+    for line in tqdm(lines):
+        tokens = line.split('\t')
+        chn_sen = tokens[1].strip().lower()
+        seg_list = jieba.cut(chn_sen)
+        length = len([w for w in seg_list])
+        lengthes.append(length)
 
-    vocab = []
-
-    print('building {} train vocab (en)')
-    for sentence in tqdm(data):
-        tokens = nltk.word_tokenize(sentence.strip().lower())
-        for token in tokens:
-            vocab.append(token)
-
-    counter_vocab = Counter(vocab)
-
-    total_count = 0
-    covered_count = 0
-    for word in tqdm(counter_vocab.keys()):
-        total_count += counter_vocab[word]
-        try:
-            temp = word_vectors[word]
-            covered_count += counter_vocab[word]
-        except (NameError, KeyError):
-            pass
-
-    print('count of words in text (en): ' + str(len(vocab)))
-    print('fasttext vocab size (en): ' + str(len(word_vectors.vocab)))
-    print('coverage: ' + str(covered_count / total_count))
+    print('max_len: ' + str(np.max(lengthes)))
 
 
 if __name__ == '__main__':
     count_train_samples()
     count_valid_samples()
-    # train_length_en()
-    # train_vocab_en()
+    count_train_length_en()
+    count_train_length_zh()
