8e31e7c437e393b48a96bcee4115cb5ba89de64d
Yang Liu <foamliu@yeah.net>
2018-09-06 16:06:07 +0800

    fix bug: [Errno 2] No such file or directory'

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ max_token_length_zh = Ty = 20 + 1   # 1 is for tailing stop word
 hidden_size = 1024
 
 
-train_folder = 'data/ai_challenger_ect2018_trainingset_20180827.zip'
-valid_folder = 'data/ai_challenger_ect2018_validationset_20180823.zip'
-test_a_folder = 'data/ai_challenger_ect2018_testA_20180827.zip'
+train_folder = 'data/ai_challenger_ect2018_trainingset_20180827'
+valid_folder = 'data/ai_challenger_ect2018_validationset_20180823'
+test_a_folder = 'data/ai_challenger_ect2018_testA_20180827'
 test_b_folder = 'data/ai_challenger_translation_test_b_20171128'
 train_translation_folder = os.path.join(train_folder, 'translation_train_20170912')
 valid_translation_folder = os.path.join(valid_folder, 'translation_validation_20170912')
