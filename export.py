4b38877b609824a2c0517d4d534f789b58f09aa2
unknown <foamliu@yeah.net>
2019-10-05 08:04:21 +0800

    update

---
+++ export.py
@@ -0,0 +1,26 @@
+import time
+
+import torch
+
+if __name__ == '__main__':
+    checkpoint = 'BEST_checkpoint.tar'
+    print('loading {}...'.format(checkpoint))
+    start = time.time()
+    checkpoint = torch.load(checkpoint)
+    print('elapsed {} sec'.format(time.time() - start))
+    model = checkpoint['model']
+    print(type(model))
+
+    filename = 'transformer.pt'
+    print('saving {}...'.format(filename))
+    start = time.time()
+    torch.save(model.state_dict(), filename)
+    print('elapsed {} sec'.format(time.time() - start))
+
+    print('loading {}...'.format(filename))
+    start = time.time()
+    from transformer import Transformer
+
+    model = Transformer()
+    model.load_state_dict(torch.load(filename))
+    print('elapsed {} sec'.format(time.time() - start))
