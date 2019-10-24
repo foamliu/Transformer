a0f746cf547419f64924fcc61b5fd11b03426d76
Yang Liu <liuyang12@focusmedia.cn>
2019-10-24 09:11:12 +0800

    update

--- transformer/optimizer.py
+++ transformer/optimizer.py
@@ -4,12 +4,11 @@ from config import d_model
 class TransformerOptimizer(object):
     """A simple wrapper class for learning rate scheduling"""
 
-    def __init__(self, optimizer, warmup_steps=4000, min_lr=1e-5):
+    def __init__(self, optimizer, warmup_steps=4000):
         self.optimizer = optimizer
         self.init_lr = d_model ** (-0.5)
         self.warmup_steps = warmup_steps
         self.lr = self.init_lr
-        self.min_lr = min_lr
         self.step_num = 0
 
     def zero_grad(self):
@@ -21,6 +20,7 @@ class TransformerOptimizer(object):
 
     def _update_lr(self):
         self.step_num += 1
+        self.min_lr = 1e-5
         self.lr = self.init_lr * min(self.step_num ** (-0.65), self.step_num * (self.warmup_steps ** (-1.5)))
         self.lr = max(self.lr, self.min_lr)
         for param_group in self.optimizer.param_groups:
